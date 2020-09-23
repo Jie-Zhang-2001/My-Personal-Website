8a6bd44e0dd72aecd50370d89f8bc453ca4162e3
Jie Zhang <jie.zhang.2@stonybrook.edu>
2020-09-23 17:00:47 -0400

    update profile link

--- src/component/menu.js
+++ src/component/menu.js
@@ -7,7 +7,7 @@ import {UsergroupDeleteOutlined} from '@ant-design/icons';
 function MenuNav() {
     return (
         <div className="leftNav">
-            <div className='profile'><img src ="/images/profile.png" /></div>
+            <div className='profile'><img src ="/image/profile.png" /></div>
             <h1 className='name'>Jie Zhang</h1>
             <Menu
                 mode="inline"
