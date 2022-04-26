3c8cbce41ccbf8b145ef2d392f668732bcdcc788
unknown <jiezhang159@gmail.com>
2022-04-26 03:07:09 -0400

    added experience

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -18,32 +18,82 @@ function About() {
       </p>
 
       <Row type="flex" justify="space-between">
-        <Col className="education" xs={24} lg={12}>
+        <Col className="education" xs={24} lg={8}>
           <h1 className="timelineTitle">Education</h1>
           <Timeline pending=" ">
             <Timeline.Item dot={<ClockCircleOutlined />}>
               <h3 className="school">Stony Brook University</h3>
               <p>2019 - 2023 Bachelor of Science in Computer Science</p>
-                          <h4>Relevant Courses:</h4>
-                          <ol>
-                              <li>
-                                  Data Structures
-                              </li>
-                              <li>
-                                  Object-Oriented Programming
-                              </li>
-                              <li>
-                                  Foundations of Computer Science (Discrete Mathematics)
-                              </li>
-                              <li>
-                                Math Courses (Linear Algebra, Calculus...)
-                              </li>
-                          </ol>
+                <h4>Relevant Courses:</h4>
+                  <ol>
+                      <li>
+                          Software Engineering
+                      </li>
+                      <li>
+                          Algorithms
+                      </li>
+                      <li>
+                          Data Structures
+                      </li>
+                      <li>
+                          Computer Networks
+                      </li>                
+                      <li>
+                          Object-Oriented Programming
+                      </li>
+                      <li>
+                          Systems Fundamentals
+                      </li>
+                  </ol>
               <h4>GPA: 3.93 / 4.0</h4>
             </Timeline.Item>
           </Timeline>
         </Col>
-        <Col xs={24} lg={12} className="experience">
+        <Col xs={24} lg={8} className="experience">
+          <h1 className="timelineTitle">Experience </h1>
+          <Timeline>
+            <Timeline.Item>
+              <h3>Interative Brokers: Software Engineer Intern</h3>
+              <p className="projectDate">
+                <CalendarOutlined /> June. 2021 - Aug. 2021
+              </p>
+              <ol>
+                      <li>
+                        Designed a web-based tool using Node.js, Vue.js, Express.js, and Elasticsearch to automate intake of Jira tickets, resulting in
+                        a 30% decrease in development time for the software engineers throughout the company.
+                      </li>
+                      <li>
+                        Developed an internal tool using Spring Boot, TypeScript, and Elasticsearch to allow designers to efficiently organize Adobe
+                        XD files, expediting design mockup requests by over 20%.
+                      </li>
+                      <li>
+                        Collaborated with UI designers and a team of 5 software engineers through daily stand-up meetings, resulting in the
+                        completion of the assigned project two weeks before the expected deadline.
+                      </li>
+                  </ol>
+            </Timeline.Item>
+            <Timeline.Item>
+              <h3>Undergraduate Teaching Assistant: Data Structures</h3>
+              <p className="projectDate">
+                <CalendarOutlined /> Jan. 2021 - June. 2021
+              </p>
+              <ol>
+                      <li>
+                          Supported over 120 students in understanding the fundamentals of data structures and algorithms, along with
+                          various paradigms and techniques of programming.
+                      </li>
+                      <li>
+                          Hosted weekly recitations that guided undergraduate students in debugging and optimizing their programs.
+                      </li>
+                      <li>
+                          Worked closely with the professor to facilitate student learning and engagement through weekly office hours and resolved
+                          additional concerns through discussion boards and emails.
+                      </li>
+                  </ol>
+            </Timeline.Item>
+          </Timeline>
+        </Col>
+        <Col xs={24} lg={8} className="experience">
           <h1 className="timelineTitle">Projects </h1>
           <Timeline pending="Please visit Projects for more...">
             <Timeline.Item>
